ec2e7936e4d94b5331d6eb5571ee57c8a558ec74
Connor Christensen <10796002@uvu.edu>
2020-10-22 12:27:11 -0600

    Added for loop of posters

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -3,15 +3,19 @@ import { people } from './data/people.js'
 
 const main = document.querySelector('main')
 
-films.forEach(film => {
-    console.log(film.title)
-    let newTitle = main.appendChild(document.createElement("h1"))
-    newTitle.textContent = film.title;
-}) 
 
-people.forEach(person => {
-    console.log(person.name)
-    let personImg = document.createdElement('img')
-    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
-    main.appendChild(personImg)
-})
+for (let i = 0; i < 7; i++) {
+    let figure = document.createElement('figure')
+
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg` // could also be done as "https://starwars-visualguide.com/assets/img/films/" + (i + 1) + ".jpg"
+
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[i].title
+
+
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+
+    main.appendChild(figure)
+}
