4e2719c0453ba5cc2aa215a7c61b7d9e4a8a4c67
Connor Christensen <10796002@uvu.edu>
2020-11-12 12:02:50 -0700

    Finishing Touches on StarWars API

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -12,7 +12,10 @@ for (let i = 0; i < 7; i++) {
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg` // could also be done as "https://starwars-visualguide.com/assets/img/films/" + (i + 1) + ".jpg"
 
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = films[i].title
+
+    const foundFilm = films.find(film => getLastNumber(film.url) == (i + 1))
+
+    figCaption.textContent = foundFilm.title
 
 
     figure.appendChild(figImg)
